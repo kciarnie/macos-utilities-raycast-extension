0a3bf84cb478cd7d10efa6d7808c87e5ae21ff17
Kevin C <kciarnie@gmail.com>
2024-01-03 23:33:26 +0100

    Restructuring the files and ading more clarity to the structures

---
+++ src/constants.tsx
@@ -0,0 +1,4 @@
+import { Cache } from "@raycast/api";
+
+export const backupLocation = "/tmp/terminal-history-backup.file.txt";
+export const cache = new Cache();
--- src/terminal-history.tsx
+++ src/terminal-history.tsx
@@ -1,135 +1,46 @@
-import { ActionPanel, Action, Icon, Color, List, confirmAlert, Alert, Cache, Detail, showHUD } from "@raycast/api";
+import { ActionPanel, Action, Icon, Color, List, confirmAlert, Alert, showHUD } from "@raycast/api";
 import { useEffect, useState } from "react";
 import { execSync } from "child_process";
-import os from 'os';
-import path from "path";
-import fs from "fs";
+import os from "os";
+import { createRaySoLink, getHistory, restoreHistory, saveFile } from "./utils";
 
-const backupLocation = "/tmp/terminal-history-backup.file.txt";
-
-const cache = new Cache();
-
-export function parseShellHistory(string: string) {
-  const reBashHistory = /^: \d+:0;/;
-
-  return string.trim().split('\n').map(line => {
-    if (reBashHistory.test(line)) {
-      return line.split(';').slice(1).join(';');
-    }
-
-    // ZSH just places one command on each line
-    return line;
-  });
-}
-
-export function shellHistoryPath({ extraPaths = [] } = {}) {
-  if (process.env.HISTFILE) {
-    return process.env.HISTFILE;
-  }
-
-  const homeDir = os.homedir();
-
-  const paths = new Set([
-    path.join(homeDir, '.bash_history'),
-    path.join(homeDir, '.zsh_history'),
-    path.join(homeDir, '.history')
-  ]);
-
-  for (const path of extraPaths) {
-    paths.add(path);
-  }
-
-  const filterdHistoryPath = () => {
-    let largestFile;
-    let size = 0;
-
-    for (const path of paths) {
-      if (!fs.existsSync(path)) {
-        continue;
-      }
-
-      if (fs.statSync(path).size > size) {
-        size = fs.statSync(path).size;
-        largestFile = path;
-      }
-    }
-    return largestFile;
-  };
-
-  return filterdHistoryPath();
-}
-
-function setHistory() {
-  const historyPath = shellHistoryPath();
-  const history = historyPath ? parseShellHistory(fs.readFileSync(historyPath).toString()) : [];
-  // Remove duplicates
-  return [...new Set(history)].map((command) => `${command}`.trim());
-}
-
-var history: string[] = [];
+let history: string[] = [];
 
 export default function Command() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(false);
-  }
-    , [loading]);
+  }, [loading]);
 
-  history = setHistory();
+  history = getHistory();
 
-  // Reverse the array
   const length = history.length;
 
-  function RestoreHistoryPanel() {
+  if (length === 0) {
     return (
-      <Action
-        title="Restore History"
-        icon={{ source: Icon.Pencil }}
-        onAction={
-          () => {
-            confirmAlert({
-              title: `Would you like to restore your history ? `,
-              primaryAction: {
-                title: "Restore",
-              },
-            }).then((confirmed) => {
-              if (confirmed) {
-                restoreHistory();
-                setLoading(true);
-              }
-            });
+      <List>
+        <List.EmptyView
+          icon={{ source: "https://placekitten.com/500/500" }}
+          title="You have no history. Run some commands to see them here."
+          actions={
+            <ActionPanel>
+              <RestoreHistoryPanel />
+            </ActionPanel>
           }
-        } shortcut={{ modifiers: ["cmd"], key: "r" }} />
+        />
+      </List>
     );
   }
 
-  if (length === 0) {
-    return <Detail
-      markdown={`No History`}
-      actions={
-        <ActionPanel>
-          <RestoreHistoryPanel />
-        </ActionPanel>
-      }
-    />
-  }
-
   return (
-    <List isLoading={history.length === 0} searchBarPlaceholder="Filter commands by name...">
+    <List searchBarPlaceholder="Filter commands by name...">
       {history.reverse().map((command, index) => (
         <List.Item
           key={index}
           title={`${command}`}
           icon={{ source: Icon.Terminal, tintColor: Color.Green }}
-          accessories={[
-            {
-              text: {
-                value: `${length - index + 1} `,
-                color: Color.PrimaryText,
-              },
-            },
-          ]}
+          accessories={[{ text: { value: `${length - index + 1}`, color: Color.PrimaryText } }]}
           actions={
             <ActionPanel>
               <Action.CopyToClipboard
@@ -151,41 +62,14 @@ export default function Command() {
                 title="Delete History"
                 shortcut={{ modifiers: ["cmd"], key: "d" }}
                 style={Action.Style.Destructive}
-                onAction={() => {
-                  confirmAlert({
-                    title: `Are you sure you want to delete your history ? `,
-                    primaryAction: {
-                      title: "Delete",
-                      style: Alert.ActionStyle.Destructive,
-                    },
-                  }).then((confirmed) => {
-                    if (confirmed) {
-                      saveFile();
-                      // Check if path exists for zsh or bash
-                      if (os.userInfo().shell === "/bin/zsh") {
-                        execSync("rm ~/.zsh_history");
-                      } else if (os.userInfo().shell === "/bin/bash") {
-                        execSync("rm ~/.bash_history");
-                      } else {
-                        console.log("Shell not supported");
-                        showHUD("Shell not supported");
-                        return;
-                      }
-                      execSync("history -c");
-                      setLoading(true);
-                    }
-                  });
-                }}
+                onAction={onDeletePressed}
               />
               <Action
                 title="Backup History"
                 icon={{ source: Icon.CopyClipboard }}
-                onAction={
-                  () => {
-                    saveFile();
-                    setLoading(true);
-                  }
-                } shortcut={{ modifiers: ["cmd"], key: "b" }} />
+                shortcut={{ modifiers: ["cmd"], key: "b" }}
+                onAction={onBackupPressed}
+              />
               <RestoreHistoryPanel />
             </ActionPanel>
           }
@@ -193,34 +77,64 @@ export default function Command() {
       ))}
     </List>
   );
-}
 
-/// Restore the history file from a backup location
-function restoreHistory() {
-  cache.get("largestFile");
-  console.log(`cp ${backupLocation} ~/.zsh_history`);
-  execSync(`cp ${backupLocation} ~/.zsh_history`);
-  setHistory();
-}
+  /// On delete pressed action
+  function onDeletePressed() {
+    confirmAlert({
+      title: `Are you sure you want to delete your history ? `,
+      primaryAction: {
+        title: "Delete",
+        style: Alert.ActionStyle.Destructive,
+      },
+    }).then((confirmed) => {
+      if (confirmed) {
+        saveFile();
+        // Check if path exists for zsh or bash
+        if (os.userInfo().shell === "/bin/zsh") {
+          execSync("rm ~/.zsh_history");
+        } else if (os.userInfo().shell === "/bin/bash") {
+          execSync("rm ~/.bash_history");
+        } else {
+          console.log("Shell not supported");
+          showHUD("Shell not supported");
+          return;
+        }
+        execSync("history -c");
+        setLoading(true);
+      }
+    });
+  }
 
-/// Save the history file to a backup location
-function saveFile() {
-  const largestFile = shellHistoryPath() ?? "";
-  cache.set("largestFile", largestFile);
-  console.log(`cp ${largestFile} ${backupLocation}`);
-  execSync(`cp ${largestFile} ${backupLocation}`);
-}
+  /// On backup pressed action
+  function onBackupPressed() {
+    saveFile();
+    setLoading(true);
+  }
 
-/// Create a ray.so link
-/// - Parameter command: The command to encode
-/// - Returns: A ray.so link
-function createRaySoLink(command: string): string {
-  try {
-    // eslint-disable-next-line no-undef
-    const decodedString = btoa(command);
-    return `https://ray.so/#language=shell&code=${decodedString}&padding=16&title=Terminal+History+Command&darkMode=true&background=true&theme=raindrop`;
-  } catch (error) {
-    console.log(`Error parsing: ${command}`);
-    return "";
+  /// On restore pressed action
+  function onRestorePressed() {
+    confirmAlert({
+      title: `Would you like to restore your history ? `,
+      primaryAction: {
+        title: "Restore",
+      },
+    }).then((confirmed) => {
+      if (confirmed) {
+        restoreHistory();
+        setLoading(true);
+      }
+    });
+  }
+
+  // The restore history panel
+  function RestoreHistoryPanel() {
+    return (
+      <Action
+        title="Restore History"
+        icon={{ source: Icon.Pencil }}
+        shortcut={{ modifiers: ["cmd", "shift"], key: "r" }}
+        onAction={onRestorePressed}
+      />
+    );
   }
 }
